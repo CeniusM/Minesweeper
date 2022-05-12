99a0d52835ccdf352bc285eb1868b2e1d6e85037
CeniusM <cenius.meier@gmail.com>
2022-05-12 22:58:19 +0200

    changes

--- Minesweeper/GUI.cpp
+++ Minesweeper/GUI.cpp
@@ -15,7 +15,7 @@ void GUI::PrintBoard()
 				if ((piece & 0b100) == 0) // not flaged
 				{
 					m_linePtr[0] = '[';
-					m_linePtr[1] = '?';
+					m_linePtr[1] = (char)219; // (char)219 = '█'
 					m_linePtr[2] = ']';
 				}
 				else
@@ -38,7 +38,7 @@ void GUI::PrintBoard()
 				m_linePtr[2] = ']';
 			}
 		}
-		MH::WriteLine(m_line);
+		std::cout << m_line << std::endl;
 	}
 }
 
@@ -46,9 +46,10 @@ GUI::GUI(int i, int j, int** board)
 {
 	count1 = i;
 	count2 = j;
-	m_line = new char[j * 3];
+	m_line = new char[j * 3 + 1];
 	for (int ii = 0; ii < j * 3; ii++)
 		m_line[ii] = (char)0;
+	m_line[j * 3] = NULL;
 	m_linePtr = nullptr;
 	this->board = board;
 }
@@ -57,3 +58,86 @@ GUI::~GUI()
 {
 	delete[] m_line;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+#include "MyHelper.cpp"
+#include "GUI.h"
+
+void GUI::PrintBoard()
+{
+	int piece;
+	for (int i = 0; i < count1; i++)
+	{
+		for (int j = 0; j < count2; j++)
+		{
+			piece = board[i][j];
+			//m_linePtr = (char*)m_line[j * 3];
+			if ((piece & 0b1) == 0) // not seen
+			{
+				if ((piece & 0b100) == 0) // not flaged
+				{
+					m_linePtr[j * 3 + 0] = '[';
+					m_linePtr[j * 3 + 1] = '?';
+					m_linePtr[j * 3 + 2] = ']';
+				}
+				else
+				{
+					m_linePtr[j * 3 + 0] = '[';
+					m_linePtr[j * 3 + 1] = 'P';
+					m_linePtr[j * 3 + 2] = ']';
+				}
+			}
+			else if ((piece & 0b10) == 0) // seen and no bomb
+			{
+				m_linePtr[j * 3 + 0] = '[';
+				m_linePtr[j * 3 + 1] = ' ';
+				m_linePtr[j * 3 + 2] = ']';
+			}
+			else // seen and bomb
+			{
+				m_linePtr[j * 3 + 0] = '[';
+				m_linePtr[j * 3 + 1] = 'X';
+				m_linePtr[j * 3 + 2] = ']';
+			}
+			std::cout << m_line[j * 3 + 0];
+			std::cout << m_line[j * 3 + 1];
+			std::cout << m_line[j * 3 + 2];
+		}
+		std::cout << std::endl;
+
+		// coudlent get it too work
+		// keept adding unwanted chars at the end
+		//MH::WriteLine(m_line);
+	}
+}
+
+GUI::GUI(int i, int j, int** board)
+{
+	count1 = i;
+	count2 = j;
+	m_line = new char[j * 3];
+	m_linePtr = (char*)m_line;
+	for (int ii = 0; ii < j * 3; ii++)
+		m_linePtr[ii] = (char)0;
+	this->board = board;
+}
+
+GUI::~GUI()
+{
+	delete[] m_line;
+}
+
+
+*/
