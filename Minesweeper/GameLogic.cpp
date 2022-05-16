13e706f1ba2a0828d120bf79e5ca4079b53316f6
CeniusM <cenius.meier@gmail.com>
2022-05-16 17:43:29 +0200

    changes

--- Minesweeper/GameLogic.cpp
+++ Minesweeper/GameLogic.cpp
@@ -2,8 +2,8 @@
 
 void GenerateBoard(int** board, int bombAmout, int startX, int startY)
 {
-		board[0][0] = 1;
-		board[4][6] = 1;
-		board[3][8] = 1;
-		board[2][3] = 1;
+	board[0][0] = 1;
+	board[4][6] = 1;
+	board[3][8] = 1;
+	board[2][3] = 1;
 }
